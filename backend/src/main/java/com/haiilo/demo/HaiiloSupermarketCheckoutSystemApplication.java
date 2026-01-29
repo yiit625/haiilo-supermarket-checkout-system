4dbf4c363a399b5f0dccc66bf7c7383c2ff9033d
yigir <yiit625@gmail.com>
2026-01-29 21:39:25 +0100

    Backend skeleton

---
+++ backend/src/main/java/com/haiilo/demo/HaiiloSupermarketCheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.haiilo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiiloSupermarketCheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiiloSupermarketCheckoutSystemApplication.class, args);
+	}
+
+}
