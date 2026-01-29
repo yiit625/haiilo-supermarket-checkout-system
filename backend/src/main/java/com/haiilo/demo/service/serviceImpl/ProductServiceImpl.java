6fb63efd0d92b1e12390c49bf0b66914f7b9cf84
yigir <yiit625@gmail.com>
2026-01-29 22:53:32 +0100

    Lombok update

--- backend/src/main/java/com/haiilo/demo/service/serviceImpl/ProductServiceImpl.java
+++ backend/src/main/java/com/haiilo/demo/service/serviceImpl/ProductServiceImpl.java
@@ -3,15 +3,13 @@ package com.haiilo.demo.service.serviceImpl;
 import com.haiilo.demo.entity.Product;
 import com.haiilo.demo.repository.ProductRepository;
 import com.haiilo.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
-
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
