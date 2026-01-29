d7b2ea9ca80abb8c6565e8290f861eb519dc08af
yigir <yiit625@gmail.com>
2026-01-29 22:52:40 +0100

    BulkOfferService & BulkOfferRepository & BulkOfferServiceImpl

--- backend/src/main/java/com/haiilo/demo/repository/BulkOfferRepository.java
+++ backend/src/main/java/com/haiilo/demo/repository/BulkOfferRepository.java
@@ -3,5 +3,8 @@ package com.haiilo.demo.repository;
 import com.haiilo.demo.entity.BulkOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BulkOfferRepository extends JpaRepository<BulkOffer, Long> {
+    Optional<BulkOffer> findByProductId(Long productId);
 }
---
+++ backend/src/main/java/com/haiilo/demo/service/serviceImpl/BulkOfferServiceImpl.java
@@ -0,0 +1,26 @@
+package com.haiilo.demo.service.serviceImpl;
+
+import com.haiilo.demo.entity.BulkOffer;
+import com.haiilo.demo.repository.BulkOfferRepository;
+import com.haiilo.demo.service.BulkOfferService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BulkOfferServiceImpl implements BulkOfferService {
+
+    private final BulkOfferRepository bulkOfferRepository;
+
+    @Override
+    public BulkOffer createOffer(BulkOffer offer) {
+        return bulkOfferRepository.save(offer);
+    }
+
+    @Override
+    public Optional<BulkOffer> getOfferByProductId(Long productId) {
+        return bulkOfferRepository.findByProductId(productId);
+    }
+}
