2536b36b5a619b40eab2640111185dec2bb42f72
yigir <yiit625@gmail.com>
2026-01-29 22:24:15 +0100

    BulkOffer && BulkOfferRepository

---
+++ backend/src/main/java/com/haiilo/demo/entity/BulkOffer.java
@@ -0,0 +1,37 @@
+package com.haiilo.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "bulk_offers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BulkOffer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne // suppose each product can have only one bulk offer
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Min(value = 1, message = "Required quantity must be at least 1")
+    private int requiredQuantity;
+
+    @NotNull(message = "Offer price is required")
+    @DecimalMin(value = "0.01", message = "Offer price must be positive")
+    private BigDecimal offerPrice;
+}
---
+++ backend/src/main/java/com/haiilo/demo/repository/BulkOfferRepository.java
@@ -0,0 +1,7 @@
+package com.haiilo.demo.repository;
+
+import com.haiilo.demo.entity.BulkOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BulkOfferRepository extends JpaRepository<BulkOffer, Long> {
+}
