b33cfbeaae8dc6da37bf40bcfb93efa1fdb61bf6
yigir <yiit625@gmail.com>
2026-01-30 14:35:47 +0100

    product service - tests - TDD

---
+++ frontend/src/app/services/product.service.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ProductService } from './product.service';
+
+describe('ProductService', () => {
+  let service: ProductService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService]
+    });
+    service = TestBed.inject(ProductService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch products from API via GET', () => {
+    const mockProducts = [
+      { id: 1, name: 'Apple', unitPrice: 0.5 },
+      { id: 2, name: 'Banana', unitPrice: 0.3 }
+    ];
+
+    service.getProducts().subscribe(products => {
+      expect(products.length).toBe(2);
+      expect(products).toEqual(mockProducts);
+    });
+
+    const req = httpMock.expectOne('http://localhost:8080/api/products');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockProducts);
+  });
+});
