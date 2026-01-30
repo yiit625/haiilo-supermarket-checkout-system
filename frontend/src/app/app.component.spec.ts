d84693cdd3467915dc9453cce7eacc6c110f018f
yigir <yiit625@gmail.com>
2026-01-30 15:19:23 +0100

    ProductListComponent test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule],
     }).compileComponents();
   });
 
--- frontend/src/app/components/product-list/product-list.component.spec.ts
+++ frontend/src/app/components/product-list/product-list.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductListComponent } from './product-list.component';
+import { ProductService } from '../../services/product.service';
+import { of } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  let productServiceMock: any;
 
   beforeEach(async () => {
+    productServiceMock = {
+      getProducts: jasmine.createSpy('getProducts').and.returnValue(of([
+        { id: 1, name: 'Apple', unitPrice: 0.5 },
+        { id: 2, name: 'Banana', unitPrice: 0.3 }
+      ]))
+    };
+
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent]
-    })
-    .compileComponents();
+      imports: [ProductListComponent, MatCardModule, HttpClientTestingModule],
+      providers: [
+        { provide: ProductService, useValue: productServiceMock }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should fetch products on init', () => {
+    expect(productServiceMock.getProducts).toHaveBeenCalled();
+    expect(component.products.length).toBe(2);
+  });
+
+  it('should render product cards', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelectorAll('mat-card').length).toBe(2);
+    expect(compiled.textContent).toContain('Apple');
   });
 });
