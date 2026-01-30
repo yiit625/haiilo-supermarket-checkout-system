2f7b22571c83b8f8faa4ff5d19afc0f074c1cb08
yigir <yiit625@gmail.com>
2026-01-30 14:20:45 +0100

    Prepared layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {CheckoutPanelComponent} from './components/checkout-panel/checkout-panel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    ProductListComponent,
+    CheckoutPanelComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
