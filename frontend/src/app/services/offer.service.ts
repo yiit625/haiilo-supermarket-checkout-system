61c3fc9360518fd31e0f8eb8576987e9377d529e
yigir <yiit625@gmail.com>
2026-01-30 16:25:56 +0100

    Offer Service ts

---
+++ frontend/src/app/services/offer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BulkOffer, BulkOfferRequest } from '../models/bulk-offer.model';
+
+@Injectable({ providedIn: 'root' })
+export class OfferService {
+  private apiUrl = 'http://localhost:8080/api/offers';
+
+  constructor(private http: HttpClient) {}
+
+  getOffers(): Observable<BulkOffer[]> {
+    return this.http.get<BulkOffer[]>(this.apiUrl);
+  }
+
+  createOffer(request: BulkOfferRequest): Observable<BulkOffer> {
+    return this.http.post<BulkOffer>(this.apiUrl, request);
+  }
+}
