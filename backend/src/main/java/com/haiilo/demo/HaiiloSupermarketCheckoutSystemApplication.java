8ade4d67637114389378cbf01cde991df2bc0859
yigir <yiit625@gmail.com>
2026-01-30 12:56:06 +0100

    EnableJpaAuditing moved to Config class

--- backend/src/main/java/com/haiilo/demo/HaiiloSupermarketCheckoutSystemApplication.java
+++ backend/src/main/java/com/haiilo/demo/HaiiloSupermarketCheckoutSystemApplication.java
@@ -2,10 +2,8 @@ package com.haiilo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // Auto Auditing by Spring instead of manuel
 public class HaiiloSupermarketCheckoutSystemApplication {
 
 	public static void main(String[] args) {
