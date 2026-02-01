578b3d3ad5d54ec36e5fce28e716eb0c50d83789
yigir <yiit625@gmail.com>
2026-02-01 11:05:11 +0100

    Product Service Test

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {Product, ProductRequest} from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ProductService {
 
   getProducts(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
+  }
+
+  createProduct(productRequest: ProductRequest): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, productRequest);
   }
 }
