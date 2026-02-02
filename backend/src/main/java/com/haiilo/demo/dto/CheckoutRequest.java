107651ebc76e5fc6dc8344390afe3d97a804bd02
yigir <yiit625@gmail.com>
2026-02-02 06:57:44 +0100

    Add limitation

--- backend/src/main/java/com/haiilo/demo/dto/CheckoutRequest.java
+++ backend/src/main/java/com/haiilo/demo/dto/CheckoutRequest.java
@@ -2,10 +2,12 @@ package com.haiilo.demo.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record CheckoutRequest(
         @NotEmpty(message = "Product list cannot be empty")
+        @Size(max = 500, message = "You cannot checkout more than 500 items at once")
         List<@NotNull(message = "Product ID cannot be null") Long> productIds
 ) {}
