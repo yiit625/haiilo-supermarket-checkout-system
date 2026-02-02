f4424efa4d74d8867ba75b5001f2cb51c024c896
yigir <yiit625@gmail.com>
2026-02-02 06:36:13 +0100

    Product service change with Pageable

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Product, ProductRequest} from '../models/product.model';
 
@@ -11,8 +11,16 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getProducts(page: number, size: number, search?: string): Observable<any> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    if (search) {
+      params = params.set('search', search);
+    }
+
+    return this.http.get<any>(this.apiUrl, { params });
   }
 
   createProduct(productRequest: ProductRequest): Observable<Product> {
